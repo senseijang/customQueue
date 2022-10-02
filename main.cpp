b34b0a480393fc62744e6c94556c98f74dd805b2
Jang, Sunwoo <andyjang@iu.edu>
2022-10-02 16:45:57 -0400

    added some prints

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@ int main()
   br();
 
   // Enqueue 5 numbers 1-5
+  std::cout << "Enqueueing 5 numbers\n";
   for (int i = 1; i < 6; i++)
   {
     myQueue.enqueue(i);
@@ -19,6 +20,7 @@ int main()
   br();
 
   // Dequeue 3 numbers, should remove 1-3
+  std::cout << "Dequeuing 3 numbers\n";
   for (int j = 0; j < 3; j++)
   {
     myQueue.dequeue();
@@ -28,6 +30,7 @@ int main()
   br();
 
   // Dequeue another 3, should remove 4,5 and throw error
+  std::cout << "Dequeuing another 3 numbers\n";
   for (int k = 0; k < 3; k++)
   {
     myQueue.dequeue();
